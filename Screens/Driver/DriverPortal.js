09e493597730b540de441595c84498beb1b930f1
AhmadSu96 <ahmad.su996@gmail.com>
2020-10-23 19:34:06 +0300

    10-23

--- Screens/Driver/DriverPortal.js
+++ Screens/Driver/DriverPortal.js
@@ -1,121 +1,61 @@
-import React, { useState } from 'react';
-import { Dimensions, TouchableOpacity, View, Text, TextInput, StyleSheet, TouchableHighlight, Platform } from 'react-native';
-import { Presets } from '../../styles';
-import Layout from '../../components/layout/Layout';
-import Icon from 'react-native-vector-icons/FontAwesome';
-// import MapView from 'react-native-maps';
-import MapboxGL from "@react-native-mapbox-gl/maps";
-MapboxGL.setAccessToken("pk.eyJ1Ijoic3VnaGFpciIsImEiOiJja2dhdG80em4wNjk3Mnh0aTlkYTMyN20yIn0.O06A55zffLE1kd-xagCuDA");
+import React, { Component } from 'react'
+import { View, Text, Dimensions } from 'react-native'
+import MapView ,{Marker} from 'react-native-maps';
 
-import I18n from '../../I18n'
-import { color } from 'react-native-reanimated';
-// import { TouchableOpacity } from 'react-native-gesture-handler';
-import { Actions } from 'react-native-router-flux';
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
-const { width, height } = Dimensions.get("screen");
-const IS_Android = Platform.OS === 'android';
+const width = Dimensions.get('window').width 
+const height = Dimensions.get('window').height
 
-// 0798449809
-export default class DriverPortal extends React.Component {
+const columbusCircleCoordinates = [
+    25.354826, 51.183884
+];
 
-    async UNSAFE_componentsWillMount() {
-        if (IS_Android) {
-            const isGranted = await MapboxGL.requestAndroidLocationPermissions();
-            this.setState({
-                isAndroidPermissionGranted: isGranted,
-                isFetchingAndroidPermission: false,
-            })
-        }
-    }
+const GooglePlacesInput = () => {
+    return (
+      <GooglePlacesAutocomplete
+        placeholder='Search'
+        onPress={(data, details = null) => {
+          // 'details' is provided when fetchDetails = true
+          console.log(data, details);
+        }}
+        query={{
+          key: 'AIzaSyDsW3S4nfyC_2IgQuu-cLxUc4xjgT91l-o',
+          language: 'en',
+        }}
+      />
+    );
+  };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            isAndroidPermissionGranted: false,
-            isFetchingAndroidPermission: IS_Android,
-            coordinates: [-122.084990, 37.426929],
-            showUserLocation: true
-        }
-
-    }
+export default class Test extends Component {
     render() {
         return (
-            <Layout>
-                <View style={styles.Continar}>
-                    <View style={styles.ButtumContinar}>
-                        <View style={styles.loginContinar}>
-                            <Text style={styles.Buttum}>Previous orders</Text>
-                        </View>
-                        <View style={styles.loginContinar}>
-                            <Text style={styles.Buttum}>Profile</Text>
-                        </View>
-                        <View style={styles.loginContinar}>
-                            <Text style={styles.Buttum}>My Rate</Text>
-                        </View>
-                    </View>
-                    <View style={{ flex: 1 }}>
-                        <MapboxGL.MapView
-                            style={styles.map}
-                            // ref={c => this._map = c}ref
-                            ref={(c) => this._map = c}
-                            zoomLevel={14}
-                            centerCoordinate={this.state.coordinates[0]}
-                            showUserLocation={true}
-                            userTrackingMode={this.state.userTrackingMode}
-                        >
-                            {/* <MapboxGL.Camera
-                                zoomLevel={16}
-                                animationMode={'flyTo'}
-                                animationDuration={0}
-                                ref={}
-                                centerCoordinate={this.state.location}
-                        >
-                        </MapboxGL.Camera>
-                        <MapboxGL.UserLocation>
-
-                        </MapboxGL.UserLocation> */}
-                        </MapboxGL.MapView>
-                    </View>
+            <View>
+                <View>
+                    <GooglePlacesInput/>
                 </View>
-            </Layout>
-        );
+                <View style={{ marginTop:50}}>
+                    <MapView
+                        style={{ height:height*0.80,width:width*1  }}
+                        initialRegion={{
+                            latitude: 25.285446,
+                            longitude: 51.531040,
+                            latitudeDelta: 0.0252,
+                            longitudeDelta: 0.0251,
+                            }}
+                    >
+                        <Marker
+                             coordinate={{latitude: 25.285446,
+                                          longitude: 51.531040}}
+                        />
+                        {/* <Callout>
+                            <Image source={{uri:data.image}}/>
+                            <Text>{data.Address}</Text>
+                        </Callout> */}
+                    {/* </Marker> */}
+                    </MapView>
+            </View>
+            </View>
+        )
     }
 }
-
-
-const styles = StyleSheet.create({
-
-    ButtumContinar: {
-        flexDirection: "row",
-        paddingTop: "5%"
-    },
-
-    loginContinar: {
-        backgroundColor: "#EF5723",
-        // width:"15%",
-        width: 100,
-        height: 45,
-        // marginTop:10,
-        alignItems: "center",
-        borderRadius: 7,
-        marginLeft: "3%",
-    },
-    Buttum: {
-        fontSize: 12,
-        paddingTop: 15,
-        color: "#fff"
-    },
-    Continar: {
-        height: '100%',
-        width: '100%',
-        backgroundColor: '#fff',
-        // alignItems:"center",
-        marginTop: -25,
-        paddingTop: "5%",
-        borderRadius: 30
-    },
-    map: {
-        flex: 1
-    }
-
-});
--- Screens/Test.js
+++ Screens/Test.js
@@ -1,36 +1,36 @@
-import React, { Component } from 'react'
-import { View, Text, Dimensions } from 'react-native'
-import MapView from 'react-native-maps';
+import Geocoder from 'react-native-geocoding';
 
-// import MapboxGL from '@react-native-mapbox-gl/maps';
 
-// MapboxGL.setAccessToken("pk.eyJ1Ijoia2Frcm90IiwiYSI6ImNrZ2dwMjZ5YjF3cXEyenF1NTF0dGp4b3UifQ.gHp3OzDUq_DUb2roh6US6w");
+Geocoder.init("AIzaSyBGWLWDLiIhZm7kshUzjdk0z-GKV3rhF6U"); // use a valid API key
 
-const columbusCircleCoordinates = [
-    25.354826, 51.183884
-];
+ 
+Geocoder.from("Colosseum")
+        .then(json => {
+            var location = json.results[0].geometry.location;
+            console.log(location);
+        })
+        .catch(error => console.warn(error));
+ 
+Geocoder.from(41.89, 12.49)
+        .then(json => {
+        		var addressComponent = json.results[0].address_components[0];
+            console.log(addressComponent);
+        })
+        .catch(error => console.warn(error));
+ 
 
-export default class Test extends Component {
-    render() {
-        return (
-            <View style={{ flex: 1 }}>
-                <MapView
-                    style={{ width: Dimensions.get('window').width, height: Dimensions.get('window').height }}
-                    initialRegion={{
-                        latitude: 25.354826,
-                        longitude: 51.183884,
-                        latitudeDelta: 0.0252,
-                        longitudeDelta: 0.0251,
-                    }}
-                    region={{
-                        latitude: 25.354826,
-                        longitude: 51.183884,
-                        latitudeDelta: 0.0252,
-                        longitudeDelta: 0.0251,
-                    }}
-                />
-                <Text>Hi</Text>
-            </View>
-        )
-    }
-}
+ 
+// location object
+Geocoder.from({
+    latitude : 41.89,
+    longitude : 12.49
+});
+ 
+// latlng object
+Geocoder.from({
+    lat : 41.89,
+    lng : 12.49
+});
+ 
+// array
+Geocoder.from([41.89, 12.49]);
